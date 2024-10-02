7531ba83477f48a12d240b3b6088d2ba30e20925
Paul Zehner <paul.zehner@cea.fr>
2024-10-02 10:32:20 +0200

    Fix old configuration

--- cmake/modules/install.cmake
+++ cmake/modules/install.cmake
@@ -2,24 +2,24 @@ include(GNUInstallDirs)
 
 install(
     TARGETS
-        dynk
+        polk
     EXPORT
-        DynkTargets
+        PolkTargets
     ARCHIVE DESTINATION
         "${CMAKE_INSTALL_LIBDIR}"
 )
 
 install(
     EXPORT
-        DynkTargets
-    NAMESPACE Dynk::
+        PolkTargets
+    NAMESPACE Polk::
     DESTINATION
-        "${CMAKE_INSTALL_LIBDIR}/Dynk"
+        "${CMAKE_INSTALL_LIBDIR}/Polk"
 )
 
 include(CMakePackageConfigHelpers)
 write_basic_package_version_file(
-    ${CMAKE_CURRENT_BINARY_DIR}/DynkConfigVersion.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/PolkConfigVersion.cmake
     VERSION ${CMAKE_PROJECT_VERSION}
     COMPATIBILITY SameMajorVersion
     ARCH_INDEPENDENT
@@ -27,7 +27,7 @@ write_basic_package_version_file(
 
 install(
     FILES
-        "${CMAKE_CURRENT_BINARY_DIR}/DynkConfigVersion.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/PolkConfigVersion.cmake"
     DESTINATION
-        "${CMAKE_INSTALL_LIBDIR}/Dynk"
+        "${CMAKE_INSTALL_LIBDIR}/Polk"
 )
